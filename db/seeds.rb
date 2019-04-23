ebfbbc3d99fd5cd2041d71cc4d0682a8e2ca1279
Faizan AH <faizuali4@gmail.com>
2019-04-23 22:05:25 +0500

    typo fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
  User.create!({name: 'Faizan Ah',provider: 'greenlight', email: 'faizuali4@gmail.com', password: '1234zxcv', role: 'admin', status: 'accepted'})
--69
