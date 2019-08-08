bfb398984bc3bf0bd39c74cc400af5886515c629
Faizan AH <faizuali4@gmail.com>
2019-08-08 10:20:44 +0500

    typo fixes

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,14 +1,14 @@
 class Platform < ApplicationRecord
   has_many :users
   has_and_belongs_to_many :bbb_servers
-  belongs_to :admin, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :admin, class_name: 'User', foreign_key: 'user_id', optional: true
 
   after_commit :set_platform
 
   private
 
   def set_platform
-    self.admin.update({platform_id: id })
+    self.admin.update({platform_id: id }) if admin.present?
   end
 
 end
